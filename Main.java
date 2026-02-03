35438cc5d3c5473ef2c7e35b3efafdf942ff292d
sean22-blip <iseandxd@gmail.com>
2026-02-03 12:43:31 +0700

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -8,11 +8,13 @@ public class Main {
 //             new int[]{50,40,30,60,45}
 //         );
         Patient patient = new Patient("John", "M", "368278", "john123@gmail.com", "123-543", med);
-       // System.out.println("Pateint name: " patient.name);
+    // System.out.println("Pateint name: " patient.name);
     // String name;
     // double price;
     // char size;
     // boolean isAvailable;
+    Medicine med = new Medicine(agrs, null, null);
+    System.out.println("Primitive copy proof");
 
     }
 }
