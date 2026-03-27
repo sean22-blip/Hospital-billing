bbf3d249dfd40f09505a05f5637bf9641850ee2d
sean22-blip <iseandxd@gmail.com>
2026-03-27 20:41:47 +0700

    ko

--- user/OwnerStaff.java
+++ user/OwnerStaff.java
@@ -1,19 +1,4 @@
 package user;
 
-public class OwnerStaff extends Staff {
-
-    public OwnerStaff(String fullName, String staffID, String phNumber, String password,
-                      String position, boolean active, String username, double salary, String email) {
-        super(fullName, staffID, phNumber, password, position, active, username, salary, email);
-    }
-
-    @Override
-    public boolean can(String action) {
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "ID: " + getStaffID() + ", Name: " + getFullname() + ", Position: " + getPosition();
-    }
+public class OwnerStaff {
 }
